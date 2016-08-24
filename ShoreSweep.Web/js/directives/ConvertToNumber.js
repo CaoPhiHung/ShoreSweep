e5ba6ff23eb307f824274a7aa14195b35c23cb9b
tommyspot <huynhquocthao@gmail.com>
2016-08-25 01:34:45 +0700

    Add directive

---
+++ ShoreSweep.Web/js/directives/ConvertToNumber.js
@@ -0,0 +1,15 @@
+﻿var ngConvertToNumber = ngConvertToNumber || angular.module('convert-to-number', []);
+
+ngGoogleMap.directive('convertToNumber', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function (val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function (val) {
+                return '' + val;
+            });
+        }
+    }
+});
